765884d4322014641bd146cf0848c813819472e5
lfrati <fratilapo@gmail.com>
2024-05-19 13:39:56 -0400

    Add undo functionality

--- docs/logic.js
+++ docs/logic.js
@@ -1,6 +1,13 @@
 const STORAGE_NAME = "qb41tc90zEkiDiOh6UcjD0ChhuSmRyfh";
+const SPELL_NAMES = get_spell_names();
+let undo = () => {};
 
-function get_cards() {
+function get_spell_names() {
+  let spells = Object.values(all_spells).sort(compare_spells);
+  return spells.map((spell) => spell.name);
+}
+
+function get_card_names() {
   let state = [];
   for (let node of document.querySelectorAll(
     "#cards-container .card #spellname"
@@ -10,34 +17,99 @@ function get_cards() {
   return state;
 }
 
-function remove_card(card) {
-  card.classList.add("removing");
-  setTimeout(() => {
-    card.remove();
+/* There are 3 where we can undo:
+- remove_card
+- remove_all
+- add_card
+*/
+function make_undo(callback) {
+  // Get the button element
+  const button = document.getElementById("undo-button");
+  // Disable the button
+  button.disabled = false;
+  undo = () => {
+    callback();
+    sort_cards();
     serialize();
-  }, 300);
+    undo = () => {};
+    button.disabled = true;
+  };
 }
 
-function remove_all() {
+function remove_card(name, undoable) {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    const h1 = card.querySelector("h1#spellname");
+    if (h1.textContent === name) {
+      card.remove();
+    }
+  });
+  // sort_cards(); // no need to sort on remove
+  serialize();
+
+  if (undoable) {
+    make_undo(() => {
+      console.log("Undoing remove", name);
+      add_card(name, false);
+    });
+  }
+}
+
+function remove_all(undoable) {
+  const names = get_card_names();
+  if (names.length == 0) {
+    return;
+  }
   let div = document.getElementById("cards-container");
   div.innerHTML = "";
+  // sort_cards(); // no need to sort on remove
+  serialize();
+
+  if (undoable) {
+    make_undo(() => {
+      console.log("Undoing remove_all", names);
+      names.forEach((name) => {
+        add_card(name, false);
+      });
+    });
+  }
 }
 
-function serialize() {
-  const state = get_cards();
-  console.log(state);
-  // Serialize the dictionary
-  const serializedState = JSON.stringify(state);
+function add_card(name, undoable) {
+  let spell = all_spells[name];
+  const container = document.getElementById("cards-container");
 
-  // Store in localStorage
-  localStorage.setItem(STORAGE_NAME, serializedState);
-  console.log("Data stored");
+  const card = document.createElement("div");
+  card.className = "card";
+  if (spell.casting_time.includes("bonus action")) {
+    card.classList.add("bonusaction");
+  } else if (spell.casting_time.includes("reaction")) {
+    card.classList.add("reaction");
+  } else {
+    card.classList.add("action");
+  }
+  card.onclick = function (event) {
+    event.preventDefault();
+    remove_card(spell.name, true);
+  };
 
-  remove_all();
-  restore();
+  card.innerHTML = make_card(spell);
+  container.appendChild(card);
+
+  sort_cards();
+  serialize();
+
+  if (undoable) {
+    make_undo(() => {
+      console.log("Undoing add", spell.name);
+      remove_card(name, false);
+    });
+  }
 }
 
-function compare(a, b) {
+function compare_spells(a, b) {
+  // order by level and then by name
   if (a.level === b.level) {
     if (a.name < b.name) {
       return -1;
@@ -50,20 +122,42 @@ function compare(a, b) {
   return a.level < b.level ? -1 : 1;
 }
 
+function compare_cards(card1, card2) {
+  return compare_spells(card2spell(card1), card2spell(card2));
+}
+
+function card2spell(card) {
+  const h1 = card.querySelector("h1#spellname");
+  let name = h1.textContent.trim();
+  return all_spells[name];
+}
+
+function sort_cards() {
+  let cards = Array.from(document.querySelectorAll(".card"));
+  cards = cards.sort(compare_cards);
+  let container = document.getElementById("cards-container");
+  container.innerHTML = "";
+  // Re-append the sorted div elements to the container
+  cards.forEach((card) => container.appendChild(card));
+}
+
+function serialize() {
+  const state = get_card_names();
+  const serializedState = JSON.stringify(state);
+  localStorage.setItem(STORAGE_NAME, serializedState);
+  console.log("Data stored: ", serializedState);
+}
+
 function restore() {
-  // Retrieve the stored string
   const serializedState = localStorage.getItem(STORAGE_NAME);
 
-  // Deserialize the string back to an object
   if (serializedState) {
     const state = JSON.parse(serializedState);
-    let spells = state.map((name) => all_spells[name]);
-    spells = spells.sort(compare);
-    console.log(spells);
     console.log("Data retrieved:", state);
-    for (let spell of spells) {
-      make_spell(spell);
+    for (let name of state) {
+      add_card(name);
     }
+    sort_cards();
   }
 }
 
@@ -110,28 +204,6 @@ function make_card(spell) {
         }`;
 }
 
-function make_spell(spell) {
-  const container = document.getElementById("cards-container");
-
-  const card = document.createElement("div");
-  card.className = "card";
-  if (spell.casting_time.includes("bonus action")) {
-    card.classList.add("bonusaction");
-  } else if (spell.casting_time.includes("reaction")) {
-    card.classList.add("reaction");
-  } else {
-    card.classList.add("action");
-  }
-  card.oncontextmenu = function (event) {
-    event.preventDefault();
-    remove_card(this);
-  };
-
-  card.innerHTML = make_card(spell);
-
-  container.appendChild(card);
-}
-
 document.addEventListener("click", function (event) {
   const suggestions = document.getElementById("suggestions");
   const searchInput = document.getElementById("searchInput");
@@ -153,11 +225,10 @@ document.querySelector("#searchInput").addEventListener("input", function () {
   const suggestions = document.getElementById("suggestions");
   suggestions.innerHTML = ""; // Clear previous suggestions
 
-  const cards = get_cards();
+  const cards = get_card_names();
 
   if (input.length > 0) {
-    suggestions.classList.remove("hidden");
-    const filteredKeys = Object.keys(all_spells).filter(
+    const filteredKeys = SPELL_NAMES.filter(
       (key) =>
         key.toLowerCase().includes(input.toLowerCase()) && !cards.includes(key)
     );
@@ -165,13 +236,10 @@ document.querySelector("#searchInput").addEventListener("input", function () {
     filteredKeys.forEach((key) => {
       const li = document.createElement("li");
       let spell = all_spells[key];
-      li.innerHTML = `<strong>${key}</strong>&nbsp&nbsp&nbsp${number_to_level(
-        spell.level
-      )}`;
+      li.innerHTML = `<strong>${key}</strong>${number_to_level(spell.level)}`;
       li.onclick = function () {
-        make_spell(spell);
+        add_card(spell.name, true);
         clear_suggestions();
-        serialize();
       };
       suggestions.appendChild(li);
     });
@@ -183,13 +251,13 @@ function number_to_level(num) {
     case 0:
       return "Cantrip";
     case 1:
-      return "1st level";
+      return "1st";
     case 2:
-      return "2nd level";
+      return "2nd";
     case 3:
-      return "3rd level";
+      return "3rd";
     default:
-      return String(num) + "th level";
+      return String(num) + "th";
   }
 }
 
